9cbecb19a295df512958bad24043bdd2d43d5576
bianca vieira <bncvr@outlook.com>
2020-08-09 19:25:46 -0300

    Consume images from cloudinary

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Study Notes`,
+    description: `Gallery with some stuff to remember`,
+    author: `@b1anca`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +27,16 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+        prefix: `notes/`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
