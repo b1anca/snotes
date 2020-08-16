36a7fff8d3a3d099c22ccde51afaf9a33b954c0a
bianca vieira <bncvr@outlook.com>
2020-08-16 20:15:49 -0300

    Add react-modal-image and remove react-zoom-pan-pinch

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,15 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import Img from "gatsby-image"
+import { Lightbox } from "react-modal-image"
 
 const Gallery = () => {
+  const [modalImageIdx, setModalImageIdx] = useState()
   const data = useStaticQuery(graphql`
     query {
       allFile(filter: { sourceInstanceName: { eq: "images/gallery" } }) {
         nodes {
           id
           name
+          publicURL
           childImageSharp {
             fluid {
               ...GatsbyImageSharpFluid
@@ -20,17 +23,32 @@ const Gallery = () => {
     }
   `)
   const images = data.allFile.nodes
+  const modalImg = images[modalImageIdx]
 
   return (
     <div>
       <Grid>
-        {images.map(image => (
-          <Img
-            key={image.id}
-            alt={image.name}
-            fluid={image.childImageSharp.fluid}
-          />
+        {images.map((image, index) => (
+          <div key={image.id} onClick={() => setModalImageIdx(index)}>
+            <Img
+              alt={image.name}
+              fluid={image.childImageSharp.fluid}
+              style={{
+                width: "100%",
+                height: "100%",
+                cursor: "pointer",
+                borderRadius: 6,
+              }}
+            />
+          </div>
         ))}
+        {modalImg && (
+          <Lightbox
+            large={modalImg.publicURL}
+            alt={modalImg.name}
+            onClose={setModalImageIdx}
+          />
+        )}
       </Grid>
     </div>
   )
